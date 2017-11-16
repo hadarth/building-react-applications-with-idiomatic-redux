3ffdb8c9ec60de6d4975bfb149bd079e4aba6281
hadarth <hadar.schindler@gmail.com>
2017-11-16 18:00:07 +0200

    added initial state option

--- todos/app.js
+++ todos/app.js
@@ -5,8 +5,23 @@ import { Provider } from 'react-redux';
 import reducer from './reducers/root';
 import TodoApp from './components/TodoApp';
 
+const initialState = {
+  todos: [
+    {
+      id: '0',
+      text: 'hello',
+      completed: false
+    },
+    {
+      id: '1',
+      text: 'hello1',
+      completed: true
+    }
+  ]
+}
+
 ReactDOM.render(
-  <Provider store={createStore(reducer)}>
+  <Provider store={createStore(reducer, initialState)}>
     <TodoApp />
   </Provider>,
   document.getElementById('app')
